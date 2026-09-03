c7ad99a5ba28aa2a02d2d36d7c3c1441980bf045
Stanislav <vasukovski@yahoo.com>
2026-09-03 19:05:00 +0100

    fixed folder js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,21 @@
-import { getImagesByQuery } from '../js/pixabay-api';
-
+import { getImagesByQuery } from './js/pixabay-api';
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from '../js/render-functions';
-
+} from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 const form = document.querySelector('.form');
-
 form.addEventListener('submit', event => {
   event.preventDefault();
-
   const query = event.currentTarget.elements['search-text'].value.trim();
-
   if (!query) {
     return;
   }
-
   clearGallery();
   showLoader();
-
   getImagesByQuery(query)
     .then(data => {
       if (data.hits.length === 0) {
